971e2d6d443b66d8846c4447a6dfa0573f46cda6
Дмитрий <clash5000@yandex.ru>
2024-03-11 12:13:16 +0400

    Добавил решения для KG_2_2 и C#_2_2 (В pr18-19_gl1_6 program, set, batch, product, element)

---
+++ C#_2_2/pr18-19_gl1_6/Element.cs
@@ -0,0 +1,70 @@
+﻿using pr18_19_gl1_6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace pr18_19_gl1_6
+{
+    [XmlInclude(typeof(Element))]
+    [Serializable]
+    public class Element : Product
+    {
+
+        private string Name;
+        private int Price;
+        private string Production_Date;
+        private string Expiry_Date;
+
+        public override int Cost
+        {
+            get
+            {
+                return Price;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return ($"Product: {Name} \n Price: {Price} \n Production Date: {Production_Date} \n Expiry Date: {Expiry_Date}");
+        }
+
+        public Element()
+        {
+
+        }
+        public Element(string name, int price, string productionDate, string expiryDate) : base(name)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Production_Date = productionDate;
+            this.Expiry_Date = expiryDate;
+        }
+
+        public override bool IsExpired(string currentDate)
+        {
+            DateTime current_Date;
+            if (DateTime.TryParse(currentDate, out current_Date))
+            {
+                DateTime expiryDate;
+                if (DateTime.TryParse(Expiry_Date, out expiryDate))
+                {
+                    return current_Date > expiryDate;
+                }
+                else
+                {
+                    Console.WriteLine($"Error parsing expiry date for {Name}");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error parsing current date for {Name}");
+                return false;
+            }
+        }
+    }
+}
---
+++ C#_2_2/pr18-19_gl1_6/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace pr18_19_gl1_6
+{
+    [XmlInclude(typeof(Element))]
+    [XmlInclude(typeof(Batch))]
+    [XmlInclude(typeof(Set))]
+    [Serializable]
+    abstract public class Product : IComparable<Product>
+    {
+
+        public string Name_Product { get; set; }
+
+        public int CompareTo(Product item)
+        {
+            return this.Cost.CompareTo(item.Cost);
+        }
+        public Product()
+        {
+
+        }
+        public Product(string name)
+        {
+            this.Name_Product = name;
+        }
+        public abstract bool IsExpired(string currentDate);
+        public abstract int Cost { get; }
+    }
+
+}
